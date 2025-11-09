8a27937126852ff80d9b17f4c374e5a7afd5b6eb
erkuan <115039617+erkuan@users.noreply.github.com>
2025-11-08 20:01:59 -0500

    Add files via upload

--- Destination.sol
+++ Destination.sol
@@ -47,12 +47,12 @@ contract Destination is AccessControl {
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		//YOUR CODE HERE
     require(underlying_tokens[_underlying_token] == address(0), "Token already registered");
-
+    
     
     // New BridgeToken
     BridgeToken bridgeToken = new BridgeToken(_underlying_token, name, symbol, msg.sender);
     bridgeToken.grantRole(bridgeToken.MINTER_ROLE(), address(this));
-    
+
     // Record
     underlying_tokens[_underlying_token] = address(bridgeToken);
     wrapped_tokens[address(bridgeToken)] = _underlying_token;
